240df23fee7fd25c1adbbdd235c22b76ff02d7e2
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-03-18 19:46:11 -0700

    attempting to resolve errors

--- clicky-game/src/components/Navbar/index.js
+++ clicky-game/src/components/Navbar/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Navbar(props) {
-    return (<nav className="navbar" >
-        Score: {props.score}
-        Top Score: {props.topScore}
+    return (<nav className="navbar">
+        Score: {props.state.score}
+        TopScore: {props.state.topScore}
     </nav >
     )
 }
